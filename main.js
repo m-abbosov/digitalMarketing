66fdb1530635e66b3a704b233f4593ffebef118d
Bakha022 <abduqahhorovbahodir536@gmail.com>
2023-10-04 16:42:49 +0500

    popup done

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-let form = document.getElementById("sheetdb-form");
+let form = document.getElementById("sheetdb-form"),
+input_name = document.querySelector(".input_name"),
+input_phone = document.querySelector(".input_phone"),
+input_company = document.querySelector(".input_company")
+open_tick = document.getElementById("popup")
 
 form.addEventListener("submit", e => {
         e.preventDefault()
-
         fetch(form.action, {
             method: "Post",
             body: new FormData(document.getElementById("sheetdb-form"))
         }).then(
             response => response.json()
         )
+        
+        open_pop()
+        render()
 })
+
+
+
+
+function open_pop() {
+    open_tick.classList.add("open-pup")
+}
+
+function close_pop() {
+    open_tick.classList.remove("open-pup")
+}
+
+
+
+
+
+function render() {
+    input_company.value = ""
+    input_name.value = ""
+    input_phone.value = ""
+}
